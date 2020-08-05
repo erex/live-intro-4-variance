c5a5b23314629a368eee2ed49c60b56464861063
Eric <ericincrail@gmail.com>
2020-08-05 15:25:25 +0100

    prep for Sept 2020

--- Prac4_solution.R
+++ Prac4_solution.R
@@ -1,46 +1,38 @@
-## ----setup, include=FALSE------------------------------------------------------------
+## ----setup, include=FALSE----------------------------------------
 knitr::opts_chunk$set(echo = TRUE, warning = FALSE, message = FALSE)
 
 
-## ---- echo=T, eval=T-----------------------------------------------------------------
+## ---- echo=T, eval=T---------------------------------------------
 library(Distance)
-# Access data
 data("Systematic_variance_2")
 conversion.factor <- convert_units("metre", "kilometre", "square kilometre")
 # Fit a simple model
 sysvar2.hn <- ds(data=Systematic_variance_2, key="hn", adjustment="cos",
                  convert.units=conversion.factor)
-# Summary
 sysvar2.hn$dht$individuals$D
 sysvar2.hn$dht$individuals$N
 
 
-## ---- quiet=T, suppress_messages=T, message=F, results=F-----------------------------
-# Bootstrap estimate of uncertainty
-# Run the bootstrap (this can take a while!) 
+## ---- quiet=T, suppress_messages=T, message=F, results=F---------
 est.boot <- bootdht(model=sysvar2.hn, flatfile=Systematic_variance_2,
                     summary_fun=bootdht_Nhat_summarize, 
                     convert.units=conversion.factor, nboot=50)
 
 
-## ---- eval=T-------------------------------------------------------------------------
-# See results
+## ---- eval=T-----------------------------------------------------
 summary(est.boot)
 
 
-## ---- echo=T, eval=T-----------------------------------------------------------------
+## ---- echo=T, eval=T---------------------------------------------
 ## Post-stratification by O2 estimator
-
 # ensure that Sample.Labels are numeric, for O2 ordering
 Systematic_variance_2$Sample.Label <- as.numeric(Systematic_variance_2$Sample.Label)
-
 # Using the Fewster et al 2009, "O2" estimator 
-est.O2 <- dht2(sysvar2.hn, flatfile=Systematic_variance_2,
-               strat_formula=~1, convert_units=conversion.factor, er_est="O2")
+est.O2 <- dht2(sysvar2.hn, flatfile=Systematic_variance_2,   strat_formula=~1, convert_units=conversion.factor, er_est="O2")
 print(est.O2, report="density")
 
 
-## ---- echo=T, eval=T-----------------------------------------------------------------
+## ---- echo=T, eval=T---------------------------------------------
 # Access the data
 data("Systematic_variance_1")
 # Ensure that Sample.Labels are numeric, for O2 ordering
